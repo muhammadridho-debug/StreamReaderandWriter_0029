2aefa02a273b4dda4b7e326795ee4bdb58fbdaf7
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-01 12:13:18 +0700

    tambah blok try berisi deklarasi array dan pemicu out of range exception

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -7,8 +7,15 @@ int main()
 {
 
     cout << "Awal program" << endl; //penanda 1
-
-    cout << "Baris program terakhir" << endl; //penanda 2
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
     
+    cout << "Baris program terakhir" << endl; //penanda 2
+
     return 0;
 }
