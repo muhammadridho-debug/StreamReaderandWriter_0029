da556dc340e86fa6fe2166641c04fc8be2236080
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-01 11:54:47 +0700

    tambah objek ofstream dan buka file dalam mode menulis

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -12,6 +12,13 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     
     return 0;
 }
