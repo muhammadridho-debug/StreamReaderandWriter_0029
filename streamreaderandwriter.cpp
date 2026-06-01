1987f2df6f31d8a6585e812bb8adfb31105b5d60
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-01 10:49:44 +0700

    deklarasi variabel baris dan membuat objek serta membuka file untuk menulis

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+    
+    //deklarasi variabel baris untuk menyimpan data string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
     return 0;
 }
