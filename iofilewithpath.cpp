3fa2d5ba33a37b2559f5c52294ea12c549449e83
muhammadridho-debug <idomuhammad11@gmail.com>
2026-06-01 12:04:01 +0700

    tampilkan seluruh isi file baris per baris dan tutup file

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -46,6 +46,13 @@ int main()
     //kondisi jika file ada
     if (infile.is_open())
     {
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai di loop
+        infile.close();
     }
     //kondisi jika file tidak ada
     else cout << "Unable to open file";
